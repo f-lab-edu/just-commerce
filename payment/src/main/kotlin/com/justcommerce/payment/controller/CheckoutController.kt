33a04f91b72334fb65c9a9f09aa9b97c3c52db3a
jeongdalma <jeongdalma@gmail.com>
2024-11-15 14:41:31 +0900

    feat: 체크아웃 진입 화면 추가 및 REST API로 수정

--- payment/src/main/kotlin/com/justcommerce/payment/controller/CheckoutController.kt
+++ payment/src/main/kotlin/com/justcommerce/payment/controller/CheckoutController.kt
@@ -1,37 +1,21 @@
 package com.justcommerce.payment.controller
 
 import com.justcommerce.payment.controller.port.CheckoutCommand
+import com.justcommerce.payment.controller.port.CheckoutResult
 import com.justcommerce.payment.controller.port.CreateCheckoutService
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class CheckoutController (
     private val createCheckoutService: CreateCheckoutService
 ) {
 
-    /**
-     * 시나리오 실행 편의를 위해 GET 메서드를 사용
-     */
-    @GetMapping("/checkout/{userId}/{cartId}")
+    @PostMapping("/checkout")
     fun checkout(
-        @PathVariable("userId") userId: Long,
-        @PathVariable("cartId") cartId: Long,
-        model: Model
-    ): String {
-        val checkoutCommand = CheckoutCommand(userId, cartId)
-        val checkoutResult = createCheckoutService.create(checkoutCommand)
-
-        model.apply {
-            this.addAttribute("orderId", checkoutResult.orderId)
-            this.addAttribute("orderName", checkoutResult.orderName)
-            this.addAttribute("amount", checkoutResult.amount)
-            this.addAttribute("customerName", checkoutResult.customerName)
-            this.addAttribute("customerEmail", checkoutResult.customerEmail)
-            this.addAttribute("customerMobilePhone", checkoutResult.customerMobilePhone)
-        }
-        return "widget/checkout"
+        @RequestBody checkoutCommand: CheckoutCommand
+    ): CheckoutResult {
+        return createCheckoutService.create(checkoutCommand)
     }
 }
