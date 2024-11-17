81812ba93e63428e8b2b69d440c7a3ff46b6f745
jeongdalma <jeongdalma@gmail.com>
2024-11-17 16:38:42 +0900

    refactor: CheckoutService 모킹

--- payment-service/build.gradle.kts
+++ payment-service/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionsSpring")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- payment-service/src/test/kotlin/com/justcommerce/payment/service/CheckoutServiceTest.kt
+++ payment-service/src/test/kotlin/com/justcommerce/payment/service/CheckoutServiceTest.kt
@@ -2,27 +2,18 @@ package com.justcommerce.payment.service
 
 import com.justcommerce.payment.controller.port.CheckoutCommand
 import com.justcommerce.payment.controller.port.CheckoutResult
-import com.justcommerce.payment.infrastructure.CartRepository
-import com.justcommerce.payment.infrastructure.UserRepository
-import com.justcommerce.payment.mock.MockUserServiceClient
-import com.justcommerce.payment.mock.SavePaymentOrderMockRepository
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class CheckoutServiceTest : DescribeSpec ({
-
-    val mockClient = MockUserServiceClient()
-    val checkoutService = CheckoutService(
-        CartRepository(mockClient),
-        UserRepository(mockClient),
-        SavePaymentOrderMockRepository()
-    )
 
     describe("체크아웃 생성 메서드는") {
 
         context("사용자 정보와 사용자의 카트가 존재한다면") {
             val userId = 1L
             val cartId = 1L
+
+            val checkoutService = MockContainer.checkoutService(userId, cartId)
             val checkoutCommand = CheckoutCommand(userId, cartId)
 
             it("체크아웃 결과를 반환한다.") {
