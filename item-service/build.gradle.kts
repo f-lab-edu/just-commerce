9bddd757afaad8555c42da92efdfa17212b1a73f
jeongdalma <jeongdalma@gmail.com>
2024-11-18 18:33:01 +0900

    chore: mockito-kotlin, asciidoctor 추가

--- item-service/build.gradle.kts
+++ item-service/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    application
+    java
     kotlin("jvm")
     kotlin("plugin.spring")
     kotlin("plugin.jpa")
@@ -10,15 +10,13 @@ plugins {
     id("io.spring.dependency-management")
 }
 
-application {
-    mainClass.set("com.justcommerce.UserApplication")
-}
-
 val snippetsDir by extra { file("build/generated-snippets") }
 val kotestVersion: String by project
 val springCloudDependenciesVersion: String by project
 val kotestExtensionsSpring: String by project
+val mockitoKotlinVersion: String by project
 dependencies {
+    implementation(project(":common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
@@ -31,6 +29,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockitoKotlinVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionsSpring")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -67,10 +66,6 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "17"
     }
-}
-
-tasks.getByName("bootJar") {
-    enabled = true
 }
 
 tasks.getByName("jar") {
--- wallet-service/build.gradle.kts
+++ wallet-service/build.gradle.kts
@@ -1,17 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    application
+    java
     kotlin("jvm")
     kotlin("plugin.spring")
     kotlin("plugin.jpa")
+    id("org.asciidoctor.jvm.convert")
     id("org.springframework.boot")
     id("io.spring.dependency-management")
 }
 
+val snippetsDir by extra { file("build/generated-snippets") }
 val kotestVersion: String by project
 val springCloudDependenciesVersion: String by project
 val kotestExtensionsSpring: String by project
+val mockitoKotlinVersion: String by project
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -27,6 +30,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockitoKotlinVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionsSpring")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
@@ -37,8 +41,24 @@ dependencyManagement {
     }
 }
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.asciidoctor {
+    dependsOn(tasks.test)
+    inputs.dir(snippetsDir)
+}
+
 tasks.withType<Test> {
+    outputs.dir(snippetsDir)
     useJUnitPlatform()
+}
+
+tasks.build {
+    dependsOn(tasks.asciidoctor)
 }
 
 tasks.withType<KotlinCompile> {
@@ -46,16 +66,6 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "17"
     }
-}
-
-allOpen {
-    annotation("jakarta.persistence.Entity")
-    annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
-}
-
-tasks.getByName("bootJar") {
-    enabled = true
 }
 
 tasks.getByName("jar") {
